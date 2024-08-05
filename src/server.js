7eb5727d256a0b386beb7cd2fc4951b034ad0e5e
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-08-05 17:37:53 +0300

    1

--- src/server.js
+++ src/server.js
@@ -19,10 +19,12 @@ export const setupServer = () => {
       },
     }),
   );
-  app.use(cors({
-    origin: 'https://back-end-aquatrack.onrender.com/',
-    credentials: true,
-  }));
+  app.use(
+    cors({
+      origin: 'http://localhost:3000/',
+      credentials: true,
+    }),
+  );
   app.use(express.json());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
