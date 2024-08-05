cf3c8648a1307ac7dfb608b4edd27706e2c8e474
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-08-05 17:57:15 +0300

    delete-sett-cors

--- src/server.js
+++ src/server.js
@@ -19,12 +19,7 @@ export const setupServer = () => {
       },
     }),
   );
-  app.use(
-    cors({
-      origin: 'http://localhost:3000/',
-      credentials: true,
-    }),
-  );
+  app.use(cors());
   app.use(express.json());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
