f6c71db2c74b275bc3dad4c76ec6430456b59b2e
Nadiia1701 <146233511+Nadiia1701@users.noreply.github.com>
2024-08-08 06:18:29 -0400

    Update auth.js

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,11 +1,11 @@
 import Joi from "joi";
 export const registerUserSchema = Joi.object({
     email: Joi.string().email().min(5).max(50).required(),
-    password: Joi.string().min(8).max(50).required(),
+    password: Joi.string().min(5).max(50).required(),
 });
 export const loginUserSchema = Joi.object({
     email: Joi.string().email().min(5).max(50).required(),
-    password: Joi.string().min(8).max(50).required(),
+    password: Joi.string().min(5).max(50).required(),
 });
 export const requestResetEmailSchema = Joi.object({
     email: Joi.string().email().required(),
