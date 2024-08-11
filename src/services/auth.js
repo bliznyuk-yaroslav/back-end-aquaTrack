c12ab1250f30cb9796b61f31760bcda4d3268f4d
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-08-11 12:07:51 +0300

    +reset-pasword

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,10 +3,18 @@ import { randomBytes } from 'crypto';
 import { hashValue, compareHash } from '../utils/hash.js';
 import { UsersCollection } from '../db/models/user.js';
 import { SessionsCollection } from '../db/models/session.js';
+import bcrypt from 'bcrypt';
 import {
   ACCESS_TOKEN_LIFETIME,
   REFRESH_TOKEN_LIFETIME,
 } from '../constant/index.js';
+import jwt from 'jsonwebtoken';
+import path from 'node:path';
+import { SMTP, TEMPLATES_DIR } from '../constant/index.js';
+import { env } from '../utils/env.js';
+import { sendEmail } from '../utils/sendMail.js';
+import handlebars from 'handlebars';
+import fs from 'node:fs/promises';
 
 const createSession = (userId) => {
   const accessToken = randomBytes(30).toString('base64');
@@ -84,4 +92,73 @@ export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
     userId: session.userId,
     ...newSession,
   });
+};
+
+export const requestResetToken = async (email) => {
+  const user = await UsersCollection.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    env('JWT_SECRET'),
+    {
+      expiresIn: '5m',
+    },
+  );
+
+  const resetPasswordTemplatePath = path.join(
+    TEMPLATES_DIR,
+    'reset-password-email.html',
+  );
+
+  const templateSource = (
+    await fs.readFile(resetPasswordTemplatePath)
+  ).toString();
+
+  const template = handlebars.compile(templateSource);
+  const html = template({
+    email: user.email,
+    link: `${env('APP_DOMAIN')}/auth/reset-password?token=${resetToken}`,
+  });
+
+  try {
+    await sendEmail({
+      from: env(SMTP.SMTP_FROM),
+      to: email,
+      subject: 'Reset your password',
+      html,
+    });
+  } catch (err) {
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+      err,
+    );
+  }
+};
+
+export const resetPassword = async (payload) => {
+  let entries;
+  try {
+    entries = jwt.verify(payload.token, env('JWT_SECRET'));
+  } catch (err) {
+    if (err instanceof Error) throw createHttpError(401, err.message);
+    throw err;
+  }
+  const user = await UsersCollection.findOne({
+    email: entries.email,
+    _id: entries.sub,
+  });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+  await UsersCollection.updateOne(
+    { _id: user._id },
+    { password: encryptedPassword },
+  );
 };
--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,16 +1,16 @@
-import Joi from "joi";
+import Joi from 'joi';
 export const registerUserSchema = Joi.object({
-    email: Joi.string().email().min(5).max(50).required(),
-    password: Joi.string().min(5).max(50).required(),
+  email: Joi.string().email().min(5).max(50).required(),
+  password: Joi.string().min(5).max(50).required(),
 });
 export const loginUserSchema = Joi.object({
-    email: Joi.string().email().min(5).max(50).required(),
-    password: Joi.string().min(5).max(50).required(),
+  email: Joi.string().email().min(5).max(50).required(),
+  password: Joi.string().min(5).max(50).required(),
 });
 export const requestResetEmailSchema = Joi.object({
-    email: Joi.string().email().required(),
+  email: Joi.string().email().required(),
 });
 export const resetPasswordSchema = Joi.object({
-    password: Joi.string().required(),
-    token: Joi.string().required(),
+  password: Joi.string().required(),
+  token: Joi.string().required(),
 });
