4b5df61bea694cb52cb4fcc4e411189cebf54c2c
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-08-13 22:46:41 +0300

    311

--- src/constant/index.js
+++ src/constant/index.js
@@ -4,7 +4,7 @@ export const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 export const genderList = ['woman', 'man'];
 
-export const ACCESS_TOKEN_LIFETIME = 15 * 60 * 1000;
+export const ACCESS_TOKEN_LIFETIME = 2 * 60 * 60 * 1000;
 
 export const REFRESH_TOKEN_LIFETIME = 30 * 24 * 60 * 60 * 1000;
 export const SMTP = {
